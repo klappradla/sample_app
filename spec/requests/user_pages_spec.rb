1b3c93e54ebffdc7c7db0910157a4ba1326d7a64
Max Mulatz <klappradla@gmail.com>
2014-02-05 14:24:59 +0100

    Leftovers from exercises of chapter 7"

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "UserPages" do
   
   subject { page }
   
-  describe "signup page" do
+  describe "signup" do
     before { visit signup_path }
     
     it { should have_content('Sign up') }
@@ -15,6 +15,12 @@ describe "UserPages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+      
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_title("Sign up") }
+        it { should have_content("error") }
       end
     end
     
@@ -27,6 +33,13 @@ describe "UserPages" do
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: "user@example.com") }
+        it { should have_title(user.name) }
+        it { should have_selector("div.alert.alert-success", text: "Welcome") }
       end
     end
   end
