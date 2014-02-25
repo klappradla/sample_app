989c4f7b79caca541f9e23adcf5a4d53175822a9
Max Mulatz <klappradla@gmail.com>
2014-02-25 19:06:39 +0100

    Following Users, tests not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_action :signed_in_user_redirect, only: [:new, :create] 
@@ -50,24 +50,39 @@ class UsersController < ApplicationController
   
   def edit
   end
-    
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
   
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
-  
-  def correct_user
+  def following
+    @title = "Following"
     @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
+    @users = @user.followed_users.paginate(page: params[:page])
+    render "show_follow"
   end
-  
-  def signed_in_user_redirect
-    unless !signed_in?
-      redirect_to root_url, notice: "Already logged in."
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def signed_in_user_redirect
+      unless !signed_in?
+        redirect_to root_url, notice: "Already logged in."
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   
@@ -26,9 +30,22 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where("user_id = ?", id)
   end
+
+  def following?(other_user)
+    self.relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    self.relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    self.relationships.find_by_followed_id(other_user.id).destroy
+  end
   
   private
-  def create_remember_token
-    self.remember_token = User.encrypt(User.new_remember_token)
-  end
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
