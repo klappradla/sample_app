5488a0363874ae88419a9b69458ee3b0e5da4cea
Max Mulatz <klappradla@gmail.com>
2014-02-26 12:12:10 +0100

    Follow/unfollow users standard-way

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -121,6 +121,18 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+
+      describe "in the relationships controller" do
+        describe "submitting the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end
     
     describe "for signed-in users" do
